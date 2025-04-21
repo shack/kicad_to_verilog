94468a054c0a3efea0fe03c0bc0882291f4e00c9
Sebastian Hack <hack@cs.uni-saarland.de>
2025-04-21 23:02:57 +0200

    wire decl, interface recognition, README missing

--- kicad_to_verilog.py
+++ kicad_to_verilog.py
@@ -1,16 +1,25 @@
+# TODO
+# - unconnected pins
+# - prefix for component name
+# - wire declarations
+
 #! /usr/bin/env python3
 
 from decimal import Decimal
 
-import sys
+from collections import defaultdict
+
 import re
+import pathlib
+import functools
 
 import tinysexpr
+import tyro
 
 def get_of(l, syms):
     for e in l:
         name = str(e[0])
-        if type(e) is list and name in syms:
+        if type(e) is list and name == syms:
             yield name, e[1:]
 
 def get(l, sym):
@@ -57,13 +66,39 @@ def mangle(s):
     if s.isidentifier():
         return s
     else:
+        s = s.replace(' ', '_')
         assert all(not c.isspace() for c in s)
         return rf'\{s} '
 
-if __name__ == "__main__":
-    with open(sys.argv[1], "r") as f:
+def get_buses(wires, bus_pattern):
+    bus = re.compile(bus_pattern)
+    buses = defaultdict(dict)
+    for w in wires:
+        if m := re.fullmatch(bus, w):
+            name, num = m.groups()
+            num = int(num)
+            buses[name][num] = w
+    return buses
+
+def main(file: pathlib.Path,
+         wires: bool = True,
+         module: bool = True,
+         bus_pattern: str = r'([/_a-zA-Z]+)(\d+)'):
+    """
+    Convert a KiCad netlist to Verilog.
+
+    Args:
+        file: The path to the netlist file.
+        wires: If True, print wire declarations.
+        module: If True, print a module declaration if the file has an interface.
+        bus_pattern: The regex pattern to match bus names and indices.
+    """
+
+    with open(file, "r") as f:
         netlist = tinysexpr.read(f, atom_handler=get_atom_handler())
         assert str(car(netlist)) == "export"
+
+    interface = None
 
     # read the components
     components = {}
@@ -71,16 +106,76 @@ if __name__ == "__main__":
         for c in get(comps, 'comp'):
             ref = get_kv(c, 'ref')
             components[ref] = get_kv(c, 'value')
+            # check all properties of the component for kicad_to_verilog fields
+            for p in get(c, 'property'):
+                n = get_kv(p, 'name')
+                v = get_kv(p, 'value')
+                if module and n == 'kicad_to_verilog' and v == 'interface':
+                    interface = ref
 
     connections = { c: {} for c in components }
+    nets = {}
     # read in the nets
-    for nets in get(netlist, 'nets'):
-        for net in get(nets, 'net'):
+    # and create a map that maps component pins to nets
+    for nets_ in get(netlist, 'nets'):
+        for net in get(nets_, 'net'):
             name = get_kv(net, 'name')
+            if not name in (vcc | gnd):
+                assert not name in nets
+                nets[name] = set(get_kv(node, 'pintype') for node in get(net, 'node'))
             for node in get(net, 'node'):
                 unit = get_kv(node, 'ref')
                 pin  = get_kv(node, 'pin')
                 connections[unit][pin] = name
+
+    # if we print a module description, we will try to find buses
+    # the following map maps signals to a pair of (bus, index)
+    # of the bus signal and the according index
+    signals_to_buses = {}
+    # print module header if interface was found
+    if interface:
+        def get_net_io(ty):
+            if any('no_connect' in s for s in ty):
+                return None
+            if 'tri_state' in ty:
+                return 'inout'
+            elif 'input' in ty:
+                assert not 'output' in ty
+                return 'input'
+            elif 'output' in ty:
+                assert not 'input' in ty
+                return 'output'
+
+        buses = get_buses(nets, bus_pattern)
+        print(f'module {mangle(components[interface])} (')
+        # get all the nets that connect to the interface
+        cs = set(connections[interface].values()) - (vcc | gnd)
+        # check if some of them are buses
+        for bus, pos_to_signal in buses.items():
+            bus_signals = { s: i for i, s in pos_to_signal.items() }
+            if bus_signals.keys() < cs:
+                # if all the signals of the bus are in the interface, we can use the bus
+                l, u = min(pos_to_signal), max(pos_to_signal)
+                tys = functools.reduce(lambda a, b: a | b, (nets[n] for n in bus_signals))
+                io = get_net_io(tys)
+                assert io, 'bus has no io type'
+                signals_to_buses |= { s: (bus, i) for s, i in bus_signals.items() }
+                print(f'  {io} [{u}:{l}] {mangle(bus)},')
+        for c in sorted(cs):
+            if not c in signals_to_buses:
+                if io := get_net_io(nets[c]):
+                    print(f'  {io} {mangle(c)},')
+        print(');')
+
+    # print wire declarations
+    if wires:
+        for w in sorted(nets):
+            if w in signals_to_buses:
+                bus, idx = signals_to_buses[w]
+                init = f' = {bus}[{idx}]'
+            else:
+                init = ''
+            print(f'  wire {mangle(w)}{init}')
 
     pulled_up = set()
     pulled_down = set()
@@ -110,7 +205,7 @@ if __name__ == "__main__":
                     assert False, 'only pull-up/down resistors are supported, ' \
                         f'got resistor between {nets[0]} and {nets[1]}'
         else:
-            print(f'{mangle(val)} {mangle(c)} (')
+            print(f'  {mangle(val)} {mangle(c)} (')
             for pin, net in pins.items():
                 if net in vcc:
                     net = "1'b1"
@@ -118,5 +213,11 @@ if __name__ == "__main__":
                     net = "1'b0"
                 else:
                     net = mangle(net)
-                print(f'  ._{(pin)}({net})')
-            print(')')
+                print(f'    ._{(pin)}({net})')
+            print('  )')
+
+    if interface:
+        print('endmodule')
+
+if __name__ == "__main__":
+    tyro.cli(main)
